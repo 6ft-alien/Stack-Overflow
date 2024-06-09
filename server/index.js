eb18787901261f0bf3b5f2362df9c92de24a8c54
6ft-alien <nityanand.exe@gmail.com>
2024-06-09 23:43:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,8 @@ dotenv.config();
 app.use(express.json({limit: "30mb", extended: true}))
 app.use(express.urlencoded({limit: "30mb", extended: true}))
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: [""],
+    methods: ["POST", "GET", "PATCH", "PUT", "DELETE"],    
     credentials: true 
   }));
 
