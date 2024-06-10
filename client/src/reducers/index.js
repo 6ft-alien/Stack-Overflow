f020c609eda1e160c8fe102b351c5fa3aeada23b
6ft-alien <nityanand.exe@gmail.com>
2024-06-10 19:29:35 +0530

    Update index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,10 +3,12 @@ import authReducer from './auth';
 import currentUserReducer from './currentUser'
 import questionsReducer from './questions'
 import usersReducer from './users';
+import loginHistoryReducer from './loginHistory';
 
 export default combineReducers({
     auth: authReducer,
     currentUserReducer,
     questionsReducer,
-    usersReducer
+    usersReducer,
+    loginHistoryReducer
 });
