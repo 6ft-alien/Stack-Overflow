5b13fc04b6976f4ba398e4f854774ebf4c078e1b
6ft-alien <nityanand.exe@gmail.com>
2024-06-10 19:41:53 +0530

    Update auth.js

--- server/models/auth.js
+++ server/models/auth.js
@@ -7,6 +7,13 @@ const userSchema = mongoose.Schema({
     about: {type: String },
     tags: {type: [String] },
     joinedOn: {type: Date, default: Date.now },
+    loginDetails: [{
+        browser: { type: String, required: true },
+        os: { type: String, required: true },
+        deviceType: { type: String, required: true },
+        ipAddress: { type: String, required: true },
+        loginTime: { type: Date, default: Date.now }
+    }]
 })
 
 export default mongoose.model("User", userSchema)
