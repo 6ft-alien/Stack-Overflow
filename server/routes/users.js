f197ca77a328019d05cafa3b12ca103b6ad0dc70
6ft-alien <nityanand.exe@gmail.com>
2024-06-13 13:23:50 +0530

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,16 @@
 import express from 'express'
-import { forgotpassword, login, resetpassword, signup } from '../controllers/auth.js'
+import { forgotpassword, login, resetpassword, signup, sendOTP, checkAuth, verifyOTP } from '../controllers/auth.js'
 import { getAllUsers, getLoginHistory, updateProfile } from '../controllers/users.js'
 import auth from '../middlewares/auth.js'
+
 
 const router = express.Router();
 
 router.post('/signup', signup)
 router.post('/login', login)
+router.post('/check-auth', checkAuth);
+router.post('/send-otp', sendOTP); 
+router.post('/verify-otp', verifyOTP);
 router.get('/login-history/:id', auth, getLoginHistory)
 router.post('/forgot-password', forgotpassword)
 router.patch('/reset-password/:id/:token', resetpassword)
