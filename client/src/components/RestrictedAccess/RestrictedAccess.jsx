df5cee2c7b245e29a360bd1c6d75b5640019b8a6
6ft-alien <nityanand.exe@gmail.com>
2024-06-14 00:45:02 +0530

    Update RestrictedAccess.jsx

--- client/src/components/RestrictedAccess/RestrictedAccess.jsx
+++ client/src/components/RestrictedAccess/RestrictedAccess.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './RestrictedAccess.css'
-
+import warning from '../../assets/warning.svg'
 const RestrictedAccess = () => (
   <section className="message-section">
     <div className="message-container">
       <div className='message-container-2'>
         <h4 className='message'>
+          <img src={warning} alt="" style={{height:"20px"}}/><br /><br />
           You are not allowed to visit<br />
           our website at this time.<br /><br />
           Try using a PC or a Laptop.<br /><br />
