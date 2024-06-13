e13ff8bcb7379af2dba1b04ae58c950801103653
6ft-alien <nityanand.exe@gmail.com>
2024-06-14 00:26:47 +0530

    Create RestrictedAccess.jsx

---
+++ client/src/components/RestrictedAccess/RestrictedAccess.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './RestrictedAccess.css'
+
+const RestrictedAccess = () => (
+  <section className="message-section">
+    <div className="message-container">
+      <div className='message-container-2'>
+        <h4 className='message'>
+          You are not allowed to visit<br />
+          our website at this time.<br /><br />
+          Try using a PC or a Laptop.<br /><br />
+          WEBSITE TIMINGS ON MOBILE:<br />
+          10:00 AM - 1:00 PM
+        </h4>
+      </div>
+    </div>
+  </section>
+);
+
+export default RestrictedAccess;
