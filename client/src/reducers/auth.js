04d935fdd9c7459baf19da0825fb64d0960d67c1
6ft-alien <nityanand.exe@gmail.com>
2024-06-13 13:19:44 +0530

    Update auth.js

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -7,16 +7,22 @@ const authReducer = (state = { authData: null, loading: false, error: null }, ac
             localStorage.clear();
             return { ...state, authData: null, loading: false, error: null };
         case 'FORGOT_PASSWORD_REQUEST':
+        case 'RESET_PASSWORD_REQUEST':
+        case 'SEND_OTP_REQUEST':
+        case 'VERIFY_OTP_REQUEST':
+        case 'CHECK_AUTH_REQUEST':
             return { ...state, loading: true, error: null };
         case 'FORGOT_PASSWORD_SUCCESS':
+        case 'RESET_PASSWORD_SUCCESS':
+        case 'SEND_OTP_SUCCESS':
+        case 'VERIFY_OTP_SUCCESS':
+        case 'CHECK_AUTH_SUCCESS':
             return { ...state, loading: false, error: null };
         case 'FORGOT_PASSWORD_FAILURE':
-            return { ...state, loading: false, error: action.error };
-        case 'RESET_PASSWORD_REQUEST':
-            return { ...state, loading: true, error: null };
-        case 'RESET_PASSWORD_SUCCESS':
-            return { ...state, loading: false, error: null };
         case 'RESET_PASSWORD_FAILURE':
+        case 'SEND_OTP_FAILURE':
+        case 'VERIFY_OTP_FAILURE':
+        case 'CHECK_AUTH_FAILURE':
             return { ...state, loading: false, error: action.error };
         default:
             return state;
