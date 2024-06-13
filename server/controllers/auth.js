36ac9d9e38044eff6c506dceae7a0f5f5ae54068
6ft-alien <nityanand.exe@gmail.com>
2024-06-13 13:22:34 +0530

    Update auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -21,38 +21,113 @@ export const signup = async (req, res) => {
     }
 }
 
-
 export const login = async (req, res) => {
     const { email, password } = req.body;
     try {
-        const existinguser = await User.findOne({ email });
-        if(!existinguser){
-            return res.status(404).json({message: "User don't exist."});
-        }
+            const existinguser = await User.findOne({ email });
+            const token = jwt.sign({ email: existinguser.email, id:existinguser._id}, process.env.JWT_SECRET, { expiresIn: '1h'});
 
-        const isPasswordCrt = await bcrypt.compare(password, existinguser.password);
-        if(!isPasswordCrt) {
-            return res.status(400).json({message: "Invalid Credentials"});
-        }
-        const token = jwt.sign({ email: existinguser.email, id:existinguser._id}, process.env.JWT_SECRET, { expiresIn: '1h'});
-        
-        const loginDetail = {
-            browser: req.useragent.browser,
-            os: req.useragent.os,
-            deviceType: req.useragent.isMobile ? 'Mobile' : (req.useragent.isDesktop ? 'Desktop' : 'Laptop'),
-            ipAddress: req.clientIp,
-            loginTime: new Date() 
-        };
-        existinguser.loginDetails.push(loginDetail);
-        await existinguser.save();
-
-        res.status(200).json({ result: existinguser, token });
+            const loginDetail = {
+                browser: req.useragent.browser,
+                os: req.useragent.os,
+                deviceType: req.useragent.isMobile ? 'Mobile' : (req.useragent.isDesktop ? 'Desktop' : 'Laptop'),
+                ipAddress: req.clientIp,
+                loginTime: new Date() 
+            };
+            existinguser.loginDetails.push(loginDetail);
+            await existinguser.save();
+    
+            res.status(200).json({ result: existinguser, token });
     }
     catch(error) {
         res.status(500).json("Something went wrong...")
     }
 };
 
+export const checkAuth = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const existinguser = await User.findOne({ email: email });
+            if(!existinguser){
+                return res.status(404).json({message: "User don't exist."});
+            }
+
+            const isPasswordCrt = await bcrypt.compare(password, existinguser.password);
+            if(!isPasswordCrt) {
+                return res.status(400).json({message: "Invalid Credentials"});
+            }
+            res.status(200).json({ success: true, message: 'Authentication successful' });
+        }
+        catch (error) {
+            console.error('Error during authentication:', error);
+            res.status(500).json({ success: false, message: 'An error occurred during authentication' });
+        }
+}
+
+export const sendOTP = async (req, res) => {
+    const { email } = req.body.email;
+    const otp = Math.floor(100000 + Math.random() * 900000); 
+    const otpExpiry = new Date(new Date().getTime() + 5 * 60000);
+
+    try {
+        await User.findOneAndUpdate({ email }, { $set: { otp, otpExpiry } }, { new: true, upsert: true });
+
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.GMAIL,
+                pass: process.env.PASS
+            }
+        });
+
+        const mailOptions = {
+            from: process.env.GMAIL,
+            to: email,
+            subject: 'Verification Code',
+            text: `Your OTP code is ${otp}`
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error('Error sending email:', error);
+                res.status(500).json({ success: false, message: 'Failed to send OTP' });
+            } else {
+                console.log('Email sent:', info.response);
+                res.status(200).json({ success: true, message: 'OTP sent successfully' });
+            }
+        });
+    } catch (error) {
+        console.error('Error sending OTP:', error);
+        res.status(500).json({ success: false, message: 'Failed to send OTP' });
+    }
+}
+
+export const verifyOTP = async (req, res) => {
+    const { email, otp } = req.body;
+
+    try {
+        const user = await User.findOne({ email: email });
+
+        if (!user) {
+            return res.status(404).json({ success: false, message: 'User not found' });
+        }
+        if (user.otp.toString() !== otp) {
+            return res.status(400).json({ success: false, message: 'Invalid OTP' });
+        }
+        if (new Date() > new Date(user.otpExpiry)) {
+            return res.status(400).json({ success: false, message: 'OTP has expired' });
+        }
+
+        user.otp = null;
+        user.otpExpiry = null;
+        await user.save();
+
+        res.status(200).json({ success: true, message: 'OTP verified successfully' });
+    } catch (error) {
+        console.error('Error during OTP verification:', error);
+        res.status(500).json({ success: false, message: 'An error occurred during OTP verification' });
+    }
+};
 
 export const forgotpassword = async (req, res) => {
     const {email} = req.body;
