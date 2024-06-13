2895d1c368b1f0be9da51f9b59d688d800f7f4e0
6ft-alien <nityanand.exe@gmail.com>
2024-06-14 00:24:20 +0530

    Create useMobileRestriction.js

---
+++ client/src/hooks/useMobileRestriction.js
@@ -0,0 +1,17 @@
+import { isMobileOnly } from 'react-device-detect';
+
+const useMobileRestriction = () => {
+    const startHour = 10;
+    const endHour = 13;
+    const now = new Date();
+    const currentHour = now.getHours();
+
+    if (isMobileOnly && (currentHour < startHour || currentHour >= endHour)) {
+        return true;
+    }
+    else {
+        return isMobileOnly;
+    }
+};
+
+export default useMobileRestriction;
