009a5159c370c8b19f5a52b2707a580403e95679
6ft-alien <nityanand.exe@gmail.com>
2024-06-14 12:30:48 +0530

    Update useMobileRestriction.js

--- client/src/hooks/useMobileRestriction.js
+++ client/src/hooks/useMobileRestriction.js
@@ -1,10 +1,15 @@
 import { isMobileOnly } from 'react-device-detect';
+import { toZonedTime } from 'date-fns-tz';
 
 const useMobileRestriction = () => {
     const startHour = 10;
     const endHour = 13;
+
+    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
     const now = new Date();
-    const currentHour = now.getHours();
+    const zonedTime = toZonedTime(now, timeZone);
+    const currentHour = zonedTime.getHours();
 
     if (isMobileOnly && (currentHour >= startHour && currentHour < endHour)) {
         return false;
