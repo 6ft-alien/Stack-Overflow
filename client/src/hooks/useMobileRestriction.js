34a5a1f772cd8911cde5c378df498c933712ac7b
6ft-alien <nityanand.exe@gmail.com>
2024-06-14 13:21:04 +0530

    Update useMobileRestriction.js

--- client/src/hooks/useMobileRestriction.js
+++ client/src/hooks/useMobileRestriction.js
@@ -1,15 +1,43 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { isMobileOnly } from 'react-device-detect';
 
 const useMobileRestriction = () => {
-    const startHour = 10;
-    const endHour = 13;
-    const now = new Date();
-    const currentHour = now.getHours();
+    const [isRestricted, setIsRestricted] = useState(false);
 
-    if (isMobileOnly && (currentHour >= startHour && currentHour < endHour)) {
-        return false;
-    }
-    return isMobileOnly;
+    useEffect(() => {
+        const fetchCurrentTime = async () => {
+            try {
+                const response = await axios.get('http://worldtimeapi.org/api/timezone/Asia/Kolkata');
+                const currentTimeIST = new Date(response.data.datetime);
+                const currentHour = currentTimeIST.getHours();
+
+                console.log(`Current Time (IST):`, currentTimeIST.toString());
+                console.log(`Current Hour in IST:`, currentHour);
+
+                const startHour = 10;
+                const endHour = 13;
+
+                if (isMobileOnly && (currentHour >= startHour && currentHour < endHour)) {
+                    setIsRestricted(false);
+                } else {
+                    if(isMobileOnly){   
+                        setIsRestricted(true);
+                    }
+                    else {
+                        setIsRestricted(false);
+                    }
+                }
+            } catch (error) {
+                console.error('Error fetching the current time:', error);
+                setIsRestricted(true);
+            }
+        };
+
+        fetchCurrentTime();
+    }, []);
+
+    return isRestricted;
 };
 
 export default useMobileRestriction;
