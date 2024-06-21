7fe771082c017015eb0f6f1b1b4ea3d9f3db9045
6ft-alien <nityanand.exe@gmail.com>
2024-06-21 09:29:45 +0530

    Update Users.jsx

--- client/src/pages/Users/Users.jsx
+++ client/src/pages/Users/Users.jsx
@@ -1,23 +1,22 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useTranslation } from 'react-i18next';
 
 import './Users.css'
 import LeftSideBar from '../../components/LeftSidebar/LeftSidebar'
 import UsersList from "./UsersList";
 
 const Users = () => {
-
-    const location = useLocation()
+    const { t } = useTranslation();
 
     return (
         <div className="home-container-1">
             <LeftSideBar />
             <div className="home-container-2" style={{marginTop: "30px"}}>
-                <h1 style={{fontWeight:"400"}}>Users</h1>
+                <h1 style={{fontWeight:"400"}}>{t('usersPage.title')}</h1>
                 <UsersList />
             </div>
         </div>
     )
 }
 
-export default Users
+export default Users;
