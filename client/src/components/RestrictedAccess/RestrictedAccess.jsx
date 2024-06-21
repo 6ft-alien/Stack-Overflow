b2e6eadd0278c6b1e2b4efa81352eabf9e5f9fc0
6ft-alien <nityanand.exe@gmail.com>
2024-06-21 09:11:25 +0530

    Update RestrictedAccess.jsx

--- client/src/components/RestrictedAccess/RestrictedAccess.jsx
+++ client/src/components/RestrictedAccess/RestrictedAccess.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
-import './RestrictedAccess.css'
-import warning from '../../assets/warning.svg'
-const RestrictedAccess = () => (
-  <section className="message-section">
-    <div className="message-container">
-      <div className='message-container-2'>
-        <h4 className='message'>
-          <img src={warning} alt="" style={{height:"20px"}}/><br /><br />
-          You are not allowed to visit<br />
-          our website at this time.<br /><br />
-          Try using a PC or a Laptop.<br /><br />
-          WEBSITE TIMINGS ON MOBILE:<br />
-          10:00 AM - 1:00 PM IST
-        </h4>
+import './RestrictedAccess.css';
+import warning from '../../assets/warning.svg';
+import { useTranslation } from 'react-i18next';
+
+const RestrictedAccess = () => {
+  const { t } = useTranslation();
+
+  return (
+    <section className="message-section">
+      <div className="message-container">
+        <div className='message-container-2'>
+          <h4 className='message'>
+            <img src={warning} alt="" style={{ height: "20px" }} /><br /><br />
+            {t('restrictedAccess.line1')}<br />
+            {t('restrictedAccess.line2')}<br /><br />
+            {t('restrictedAccess.line3')}<br /><br />
+            {t('restrictedAccess.line4')}<br />
+            {t('restrictedAccess.line5')}
+          </h4>
+        </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default RestrictedAccess;
