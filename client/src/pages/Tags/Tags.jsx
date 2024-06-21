2b46d175eea9813721d3bf261a289b5e2ae5c6a7
6ft-alien <nityanand.exe@gmail.com>
2024-06-21 09:24:59 +0530

    Update Tags.jsx

--- client/src/pages/Tags/Tags.jsx
+++ client/src/pages/Tags/Tags.jsx
@@ -1,27 +1,31 @@
 import React from "react";
+import { useTranslation } from 'react-i18next';
 import TagsList from "./TagsList";
-import LeftSideBar from '../../components/LeftSidebar/LeftSidebar'
-import { tagsList } from './tagList'
-import './Tags.css'
+import LeftSideBar from '../../components/LeftSidebar/LeftSidebar';
+import { useTagsList } from './tagList';
+import './Tags.css';
 
 const Tags = () => {
+    const { t } = useTranslation();
+    const tagsList = useTagsList();
+    
     return (
         <div className="home-container-1">
             <LeftSideBar />
             <div className="home-container-2">
-                <h1 className="tags-h1">Tags</h1>
-                <p className="tags-p">A tag is a keyword or label that categorizes your question with other, similar questions.</p>
-                <p className="tags-p">Using the right tags makes it easier for others to find and answer your question.</p>
+                <h1 className="tags-h1">{t('tagPage.tags')}</h1>
+                <p className="tags-p">{t('tagPage.description1')}</p>
+                <p className="tags-p">{t('tagPage.description2')}</p>
                 <div className="tags-list-container">
                     {
                         tagsList.map((tag) => (
-                            <TagsList tag={tag} key={tagsList.id}/>
+                            <TagsList tag={tag} key={tag.tagName}/>
                         ))
                     }
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Tags
+export default Tags;
