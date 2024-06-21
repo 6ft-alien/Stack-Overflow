f026f511396039bcc28e11c8b6161522a84fd530
6ft-alien <nityanand.exe@gmail.com>
2024-06-21 09:20:08 +0530

    Update Auth.jsx

--- client/src/pages/Auth/Auth.jsx
+++ client/src/pages/Auth/Auth.jsx
@@ -5,11 +5,11 @@ import { useNavigate, Link } from 'react-router-dom';
 import './Auth.css';
 import AboutAuth from './AboutAuth';
 import { signup, login, checkAuth, sendOTP, verifyOTP } from '../../actions/auth';
-import { isChrome } from 'react-device-detect'
+import { isChrome } from 'react-device-detect';
+import { useTranslation } from 'react-i18next';
 
 const Auth = () => {
-
-  
+  const { t } = useTranslation();
   const [isSignup, setIsSignup] = useState(false);
   const [isOTPSent, setIsOTPSent] = useState(false);
   const [name, setName] = useState('');
@@ -29,115 +29,106 @@ const Auth = () => {
     e.preventDefault();
     if (isSignup) {
       if (!email && !password && !name) {
-        valid=false;
-        alert('Enter Name, Email & Password');
+        valid = false;
+        alert(t('authPage.alerts.enterNameEmailPassword'));
         return;
       }
       if (!email && password && !name) {
-        alert('Enter Name & Email');
-        valid=false;
+        alert(t('authPage.alerts.enterNameEmail'));
+        valid = false;
         return;
       }
     }
     if (!email && !password) {
-      alert('Enter Email & Password');
-      valid=false;
+      alert(t('authPage.alerts.enterEmailPassword'));
+      valid = false;
       return;
     }
     if (!email && password) {
-      alert('Enter Email');
-      valid=false;
+      alert(t('authPage.alerts.enterEmail'));
+      valid = false;
       return;
     }
     if (email && !password) {
-      alert('Enter Password');
-      valid=false;
+      alert(t('authPage.alerts.enterPassword'));
+      valid = false;
       return;
     }
     if (isSignup) {
       if (!name) {
-        alert('Enter Name');
-        valid=false;
+        alert(t('authPage.alerts.enterName'));
+        valid = false;
         return;
       }
-      if (password.length<8 || !/\d/.test(password) || !/[a-zA-Z]/.test(password)) {
-        alert('Password must contain at least eight characters including at least 1 letter and 1 number');
-        valid=false;
+      if (password.length < 8 || !/\d/.test(password) || !/[a-zA-Z]/.test(password)) {
+        alert(t('authPage.alerts.passwordRequirements'));
+        valid = false;
       }
       if (valid === true) {
         const response = await dispatch(signup({ name, email, password }, navigate));
-      if (!response.success) {
-        alert(response.message);
-      } else {
-        alert('Registration successful!');
+        if (!response.success) {
+          alert(response.message);
+        } else {
+          alert(t('authPage.alerts.registrationSuccess'));
+        }
       }
-      }
-    } 
-    else {
-      if(isChrome) {
-        if(isOTPSent) {
-          if(!otp) {
-            alert('OTP is Required')
+    } else {
+      if (isChrome) {
+        if (isOTPSent) {
+          if (!otp) {
+            alert(t('authPage.alerts.otpRequired'));
             return;
           }
-          const verified = await dispatch(verifyOTP({ email, otp }))
-            if(!verified.success) {
-              alert(verified.message)
+          const verified = await dispatch(verifyOTP({ email, otp }));
+          if (!verified.success) {
+            alert(verified.message);
+          } else {
+            const responseChrome = await dispatch(login({ email, password }, navigate));
+            if (!responseChrome.success) {
+              alert(responseChrome.message);
+            } else {
+              alert(t('authPage.alerts.loginSuccess'));
             }
-            else {
-              const responseChrome = await dispatch(login({ email, password }, navigate));
-              if (!responseChrome.success) {
-                alert(responseChrome.message);
-              } else {
-                alert('Login successful!');
-              }
+          }
+        } else {
+          const check = await dispatch(checkAuth({ email, password }));
+          if (!check.success) {
+            alert(check.message);
+          } else {
+            const response = await dispatch(sendOTP({ email }));
+            if (!response.success) {
+              alert(response.message);
+            } else {
+              alert(t('authPage.alerts.otpSent'));
+              setIsOTPSent(!isOTPSent);
             }
+          }
         }
-        else {
-          const check = await dispatch(checkAuth({ email, password }))
-          if(!check.success)
-            {
-              alert(check.message)
-            }
-            else {
-              const response = await dispatch(sendOTP({ email }))
-                if(!response.success) {
-                  alert(response.message)
-                }
-                else {
-                  alert('An OTP has been sent to your mail for verification');
-                  setIsOTPSent(!isOTPSent);
-                }
-            }
-        }
-      }
-      else {
-          const check = await dispatch(checkAuth({ email, password }))
-          if(!check.success)
-            {
-              alert(check.message)
-            }
-          else {
-              const response = await dispatch(login({ email, password }, navigate));
-              if (!response.success) {
-                alert(response.message);
-              } else {
-                alert('Login successful!');
-              }
+      } else {
+        const check = await dispatch(checkAuth({ email, password }));
+        if (!check.success) {
+          alert(check.message);
+        } else {
+          const response = await dispatch(login({ email, password }, navigate));
+          if (!response.success) {
+            alert(response.message);
+          } else {
+            alert(t('authPage.alerts.loginSuccess'));
           }
         }
       }
-  }
+    }
+  };
 
   return (
     <section className="auth-section">
       {isSignup && <AboutAuth />}
       <div className="auth-container-2">
         {!isSignup && <img src={icon} alt="stack overflow" className="login-logo" />}
-        <form onSubmit={handleSubmit} style={isSignup ? { marginTop: "60px"}:{}}>
+        <form onSubmit={handleSubmit} style={isSignup ? { marginTop: "60px", width: "300px" } : {}}>
           {isSignup && (
             <label htmlFor="name">
-              <h4>Display Name</h4>
+              <h4>{t('authPage.labels.displayName')}</h4>
               <input
                 type="text"
                 id="name"
@@ -148,7 +139,7 @@ const Auth = () => {
             </label>
           )}
           <label htmlFor="email">
-            <h4>Email</h4>
+            <h4>{t('authPage.labels.email')}</h4>
             <input
               type="email"
               name="email"
@@ -160,10 +151,10 @@ const Auth = () => {
           </label>
           <label htmlFor="password">
             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-              <h4>Password</h4>
+              <h4>{t('authPage.labels.password')}</h4>
               {!isSignup && (
                 <Link className='forgot' to={'/forgot-password'}>
-                  <p style={{ color: '#007ac6', fontSize: '13px' }}>Forgot Password?</p>
+                  <p style={{ color: '#007ac6', fontSize: '13px' }}>{t('authPage.forgotPasswordLink')}</p>
                 </Link>
               )}
             </div>
@@ -177,61 +168,53 @@ const Auth = () => {
             />
             {isSignup && (
               <p style={{ color: '#666767', fontSize: '13px' }}>
-                Passwords must contain at least eight
-                <br />
-                characters, including at least 1 letter and 1
-                <br />
-                number.
+                {t('authPage.passwordRequirements')}
               </p>
             )}
           </label>
 
           {(isChrome && !isSignup) && (
             <label htmlFor="otp">
-            <h4>OTP</h4>
-            <input
-              className='otp-field'
-              type="text"
-              name="otp"
-              id="otp"
-              disabled={!isOTPSent}
-              value={otp}
-              onChange={(e) => setOTP(e.target.value)}
-            />
-          </label>
+              <h4>{t('authPage.labels.otp')}</h4>
+              <input
+                className='otp-field'
+                type="text"
+                name="otp"
+                id="otp"
+                disabled={!isOTPSent}
+                value={otp}
+                onChange={(e) => setOTP(e.target.value)}
+              />
+            </label>
           )}
 
           {isSignup && (
             <label htmlFor="check">
               <input type="checkbox" id="check" name="check" />
               <p style={{ fontSize: '13px' }}>
-                Opt-in to receive occasional,
-                <br />
-                product updates, user research invitations,
-                <br />
-                company announcements, and digests.
+                {t('authPage.checkBoxText')}
               </p>
             </label>
           )}
           <button type="submit" className="auth-btn">
             {isChrome ? (
-              isSignup ? 'Sign up' : isOTPSent ? 'Login' : 'Send OTP'
+              isSignup ? t('authPage.buttons.signup') : isOTPSent ? t('authPage.buttons.login') : t('authPage.buttons.sendOTP')
             ) :
-            ( isSignup ? 'Sign up' : 'Login' )}
+              (isSignup ? t('authPage.buttons.signup') : t('authPage.buttons.login'))}
           </button>
           {isSignup && (
             <p style={{ color: '#666767', fontSize: '13px' }}>
-              By clicking "Sign up", you agree to our
-              <span style={{ color: '#007ac6' }}> terms of<br />service</span>,
-              <span style={{ color: '#007ac6' }}> privacy policy</span> and
-              <span style={{ color: '#007ac6' }}> cookie policy</span>
+              {t('authPage.termsOfService')} 
+              <span style={{ color: '#007ac6' }}> {t('authPage.termsLinks.terms')}</span>,
+              <span style={{ color: '#007ac6' }}> {t('authPage.termsLinks.privacyPolicy')}</span> {t('authPage.and')}
+              <span style={{ color: '#007ac6' }}> {t('authPage.termsLinks.cookiePolicy')}</span>.
             </p>
           )}
         </form>
         <p>
-          {isSignup ? 'Already have an account?' : "Don't have an account?"}
+          {isSignup ? t('authPage.alreadyHaveAccountText') : t('authPage.dontHaveAccountText')}
           <button type="button" className="handle-switch-btn" onClick={handleSwitch}>
-            {isSignup ? 'Log in' : 'Sign up'}
+            {isSignup ? t('authPage.switchButtonText.login') : t('authPage.switchButtonText.signup')}
           </button>
         </p>
       </div>
@@ -240,4 +223,3 @@ const Auth = () => {
 };
 
 export default Auth;
-  
