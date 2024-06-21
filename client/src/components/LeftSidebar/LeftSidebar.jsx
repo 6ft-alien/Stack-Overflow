81e63473827b4b19d33068794ed5fbe9c65f518e
6ft-alien <nityanand.exe@gmail.com>
2024-06-21 09:08:51 +0530

    Update LeftSidebar.jsx

--- client/src/components/LeftSidebar/LeftSidebar.jsx
+++ client/src/components/LeftSidebar/LeftSidebar.jsx
@@ -2,25 +2,29 @@ import React from 'react'
 import './LeftSidebar.css'
 import { NavLink } from 'react-router-dom'
 import Globe from '../../assets/Globe.svg'
+import { useTranslation } from 'react-i18next';
+
 const LeftSidebar = () => {
+  const { t } = useTranslation();
+
   return (
     <div className='left-sidebar'>
       <nav className="side-nav">
         <NavLink to='/' className='side-nav-links' activeClassName='active'>
-          <p>Home</p>
+          <p>{t('leftbar.home')}</p>
         </NavLink>
         <div className='side-nav-div'>
-          <div><p>PUBLIC</p></div>
+          <div><p>{t('leftbar.public')}</p></div>
           <NavLink to='/Questions' className='side-nav-links' activeClassName='active' >
             <img src={Globe} alt="Globe" />
-            <p style={{paddingLeft: "10px"}}>Questions</p>
+            <p style={{paddingLeft: "10px"}}>{t('leftbar.questions')}</p>
           </NavLink>
           
           <NavLink to='/Tags' className='side-nav-links' activeClassName='active' style={{paddingLeft: "40px"}}>
-            <p>Tags</p>
+            <p>{t('leftbar.tags')}</p>
           </NavLink>
           <NavLink to='/Users' className='side-nav-links' activeClassName='active' style={{paddingLeft: "40px"}}>
-            <p>Users</p>
+            <p>{t('leftbar.users')}</p>
           </NavLink>
         </div>
       </nav>      
