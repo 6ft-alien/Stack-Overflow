0a168ceb9c47d23c296a60aef8840bd0b79891d4
6ft-alien <nityanand.exe@gmail.com>
2024-06-21 09:26:00 +0530

    Update LoginHistory.jsx

--- client/src/pages/UserProfile/LoginHistory.jsx
+++ client/src/pages/UserProfile/LoginHistory.jsx
@@ -1,18 +1,19 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import { useTranslation } from 'react-i18next';
 import LeftSideBar from '../../components/LeftSidebar/LeftSidebar';
 import LoginHistoryList from "./LoginHistoryList";
 import { fetchLoginHistory } from "../../actions/users";
-import './LoginHistory.css'
-
+import './LoginHistory.css';
 
 const LoginHistory = () => {
     const { id } = useParams();
     const dispatch = useDispatch();
+    const { t } = useTranslation();
 
     const users = useSelector((state) => state.usersReducer);
-    const currentProfile = users.filter((user) => user._id === id )[0];
+    const currentProfile = users.find((user) => user._id === id);
     const loginHistory = useSelector((state) => state.loginHistoryReducer);
 
     useEffect(() => {
@@ -25,11 +26,11 @@ const LoginHistory = () => {
         <div className="home-container-1">
             <LeftSideBar />
             <div className="home-container-2" style={{ marginTop: "30px" }}>
-                <h1 style={{ fontWeight: "400" }}><b>{currentProfile?.name}</b> - Login History</h1>
+                <h1 style={{ fontWeight: "400" }}><b>{currentProfile?.name}</b> - {t('loginHistory.loginHistory')}</h1>
                 <LoginHistoryList loginHistory={loginHistory} />
             </div>
         </div>
     );
-}
+};
 
 export default LoginHistory;
