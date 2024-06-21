f1bf954d2d399533765758c3491cc6b08a6f5c01
6ft-alien <nityanand.exe@gmail.com>
2024-06-21 09:30:25 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import { useDispatch } from 'react-redux';
 import AllRoutes from './AllRoutes';
 import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom'
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 import { fetchAllUsers } from './actions/users';
 function App() {
 
@@ -17,10 +19,12 @@ function App() {
 
   return (
     <div className='App'>
+      <I18nextProvider i18n={i18n}>
       <Router>
         <Navbar />
         <AllRoutes />
       </Router>
+      </I18nextProvider>
     </div>
   );
 }
