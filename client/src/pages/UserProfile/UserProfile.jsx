6b0b382c7eba97a021340b96a3a61484d04549fd
6ft-alien <nityanand.exe@gmail.com>
2024-06-21 09:28:43 +0530

    Update UserProfile.jsx

--- client/src/pages/UserProfile/UserProfile.jsx
+++ client/src/pages/UserProfile/UserProfile.jsx
@@ -1,29 +1,30 @@
 import React, { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { useSelector } from 'react-redux'
-import moment from 'moment'
+import { useSelector } from 'react-redux';
+import moment from 'moment';
+import { useTranslation } from 'react-i18next';
 
-import pen from '../../assets/pen-solid.svg'
-import info from '../../assets/info.svg'
-import cake_candles from '../../assets/cake_candles.svg'
-import LeftSideBar from '../../components/LeftSidebar/LeftSidebar'
-import Avatar from '../../components/Avatar/Avatar'
+import pen from '../../assets/pen-solid.svg';
+import info from '../../assets/info.svg';
+import cake_candles from '../../assets/cake_candles.svg';
+import LeftSideBar from '../../components/LeftSidebar/LeftSidebar';
+import Avatar from '../../components/Avatar/Avatar';
 import EditProfileForm from "./EditProfileForm";
 import ProfileBio from "./ProfileBio";
-import './UsersProfile.css'
+import './UsersProfile.css';
 
 const UserProfile = () => {
+    const { t } = useTranslation();
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const users = useSelector((state) => state.usersReducer);
+    const currentProfile = users.filter((user) => user._id === id )[0];
+    const currentUser = useSelector((state) => state.currentUserReducer);
 
-    const { id } = useParams()
-    const navigate = useNavigate()
-    const users = useSelector((state) => state.usersReducer)
-    const currentProfile = users.filter((user) => user._id === id )[0]
-    const currentUser = useSelector((state) => state.currentUserReducer)
-
-    const [Switch, setSwitch] = useState(false)
+    const [Switch, setSwitch] = useState(false);
 
     const handleLoginHistory = () => {
-        navigate(`/Users/login-history/${id}`)
+        navigate(`/Users/login-history/${id}`);
     }
 
     return (
@@ -33,25 +34,24 @@ const UserProfile = () => {
                 <section>
                     <div className="user-details-container">
                         <div className="user-details">
-                            <Avatar borderRadius="10px" color="white" fontSize="50px" size="125px" >
+                            <Avatar borderRadius="10px" color="white" fontSize="50px" size="125px">
                                 {currentProfile?.name.charAt(0).toUpperCase()}
                             </Avatar>
                             <div className="user-name">
                                 <h1>{currentProfile?.name}</h1>
-                                <p><img src={cake_candles} alt="Cake" style={{height:"13px"}} /> Joined {moment(currentProfile?.joinedOn).fromNow()}</p>
+                                <p><img src={cake_candles} alt="Cake" style={{height:"13px"}} /> {t('userProfile.joined')} {moment(currentProfile?.joinedOn).fromNow()}</p>
                             </div>
                         </div>
                         {
                             currentUser?.result._id === id && (
                                 <div>
                                     <button type='button' onClick={() => setSwitch(true)} className="edit-profile-btn">
-                                        <img src={pen} alt="" style={{height:"13px", marginRight:"5px"}}/> Edit Profile
+                                        <img src={pen} alt="" style={{height:"13px", marginRight:"5px"}}/> {t('userProfile.editProfile')}
                                     </button>
                                     <button type='button' onClick={() => handleLoginHistory()} className="edit-profile-btn">
-                                        <img src={info} alt="" style={{height:"13px", marginRight:"5px"}}/> Login History
+                                        <img src={info} alt="" style={{height:"13px", marginRight:"5px"}}/> {t('userProfile.loginHistory')}
                                     </button>
                                 </div>
-                                
                             )
                         }
                     </div>
@@ -70,4 +70,4 @@ const UserProfile = () => {
     )
 }
 
-export default UserProfile 
+export default UserProfile;
