598ca73ef456bf8c981437c1e1764f2590a0f935
6ft-alien <nityanand.exe@gmail.com>
2024-06-21 09:12:21 +0530

    Update Widget.jsx

--- client/src/components/RightSidebar/Widget.jsx
+++ client/src/components/RightSidebar/Widget.jsx
@@ -1,57 +1,60 @@
-import React from 'react'
-import './RightSidebar.css'
-import comment from '../../assets/comment-alt-solid.svg'
-import pen from '../../assets/pen-solid.svg'
-import blackLogo from '../../assets/blacklogo.svg'
+import React from 'react';
+import './RightSidebar.css';
+import comment from '../../assets/comment-alt-solid.svg';
+import pen from '../../assets/pen-solid.svg';
+import blackLogo from '../../assets/blacklogo.svg';
+import { useTranslation } from 'react-i18next';
 
 const Widget = () => {
+  const { t } = useTranslation();
+
   return (
     <div className='widget'>
-      <h4>The Overflow Blog</h4>      
+      <h4>{t('rightbar.blogTitle')}</h4>
       <div className="right-sidebar-div-1">
         <div className="right-sidebar-div-2">
           <img src={pen} alt="pen" width='18'/>
-          <p>Observability is key to the future of software (and your DevOps career)</p>
+          <p>{t('rightbar.blog1')}</p>
         </div>
         <div className="right-sidebar-div-2">
           <img src={pen} alt="pen" width='18'/>
-          <p>Podcast 374: How valuable is your screen name?</p>
+          <p>{t('rightbar.blog2')} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
         </div>
       </div>
 
-      <h4>Featured on Meta</h4>      
+      <h4>{t('rightbar.metaTitle')}</h4>
       <div className="right-sidebar-div-1">
         <div className="right-sidebar-div-2">
           <img src={comment} alt="comment" width='18'/>
-          <p>Review queue workflows - Final release...</p>
+          <p>{t('rightbar.meta1')} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
         </div>
         <div className="right-sidebar-div-2">
           <img src={comment} alt="comment" width='18'/>
-          <p>Please welcome Valued Associates: #958 - V2Blast #959 - SpencerG</p>
+          <p>{t('rightbar.meta2')}</p>
         </div>
         <div className="right-sidebar-div-2">
           <img src={blackLogo} alt="blackLogo" width='18'/>
-          <p>Outdated Answers: accepted answer is now unpinned on Stack Overflow</p>
+          <p>{t('rightbar.meta3')}</p>
         </div>
       </div>
 
-      <h4>Hot Meta Posts</h4>      
+      <h4>{t('rightbar.hotMetaTitle')}</h4>
       <div className="right-sidebar-div-1">
         <div className="right-sidebar-div-2">
           <p>38</p>
-          <p>Why was this spam flag declined, yet the question marked as spam?</p>
+          <p>{t('rightbar.hotMeta1')}</p>
         </div>
         <div className="right-sidebar-div-2">
           <p>20</p>
-          <p>What is the best course of action when a user has a high enough rep to...</p>
+          <p>{t('rightbar.hotMeta2')}</p>
         </div>
         <div className="right-sidebar-div-2">
           <p>14</p>
-          <p>Is a link to the "How to ask" help page a useful comment?</p>
+          <p>{t('rightbar.hotMeta3')}</p>
         </div>
       </div>
     </div>
   )
 }
 
-export default Widget
+export default Widget;
