e01a4f7a33e90284acb591e48f9e849d8f3d4435
6ft-alien <nityanand.exe@gmail.com>
2024-06-21 09:07:51 +0530

    Update Questions.jsx

--- client/src/components/HomeMainbar/Questions.jsx
+++ client/src/components/HomeMainbar/Questions.jsx
@@ -1,35 +1,38 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import moment from 'moment'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+import { useTranslation } from 'react-i18next';
 
-const Questions = ({question}) => {
+const Questions = ({ question }) => {
+  const { t } = useTranslation();
+
   return (
     <div className='display-question-container'>
       <div className="display-votes-ans">
         <p>{question.upVote.length - question.downVote.length}</p>
-        <p>votes</p>
+        <p>{t('quesList.votes')}</p>
       </div>
       <div className="display-votes-ans">
         <p>{question.noOfAnswers}</p>
-        <p>answers</p>
+        <p>{t('quesList.answers')}</p>
       </div>
       <div className="display-question-details">
         <Link to={`/Questions/${question._id}`} className='question-title-link'>{question.questionTitle}</Link>
         <div className="display-tags-time">
           <div className="display-tags">
             {
-              question.questionTags.map((ele) => (
-                <p key={ele}>{ele}</p>
+              question.questionTags.map((tag) => (
+                <p key={tag}>{tag}</p>
               ))
             }
           </div>
           <div className="display-time">
-            asked { moment(question.askedOn).fromNow() } - { question.userPosted }
+            {t('quesList.asked', { time: moment(question.askedOn).fromNow(), user: question.userPosted })}
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Questions
+export default Questions;
