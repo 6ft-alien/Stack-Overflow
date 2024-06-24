9c56c9006f6e534d6499f12298b8d0b7b02485fb
6ft-alien <nityanand.exe@gmail.com>
2024-06-24 23:00:32 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,9 @@ import AllRoutes from './AllRoutes';
 import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom'
 import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
 import { fetchAllUsers } from './actions/users';
+import i18n from './hooks/i18n';
+import LanguageBackground from './components/LanguageBackground/LanguageBackground';
 function App() {
 
   const dispatch = useDispatch()
@@ -18,14 +19,14 @@ function App() {
   }, [dispatch])
 
   return (
-    <div className='App'>
-      <I18nextProvider i18n={i18n}>
+    <I18nextProvider i18n={i18n}>
       <Router>
-        <Navbar />
-        <AllRoutes />
+        <LanguageBackground>
+          <Navbar />
+          <AllRoutes />
+        </LanguageBackground>
       </Router>
-      </I18nextProvider>
-    </div>
+    </I18nextProvider>
   );
 }
 
