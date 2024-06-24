c8d859b1f533459968e18d1c2e8f8740bb3963d9
6ft-alien <nityanand.exe@gmail.com>
2024-06-24 22:52:02 +0530

    Update ForgotPassword.jsx

--- client/src/pages/Auth/ForgotPassword.jsx
+++ client/src/pages/Auth/ForgotPassword.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import icon from '../../assets/icon.svg';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import './Auth.css';
@@ -9,9 +9,17 @@ import { forgotPassword } from '../../actions/auth';
 const ForgotPassword = () => {
     const { t } = useTranslation();
     const [email, setEmail] = useState('');
+    const [language, setLanguage] = useState('');
 
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const location = useLocation();
+
+    useEffect(() => {
+        const lang = location.state?.language || localStorage.getItem('language');
+        setLanguage(lang);
+    }, [location]);
+    
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -31,7 +39,7 @@ const ForgotPassword = () => {
     };
 
     return (
-        <section className="auth-section">
+        <section className={`auth-section ${language}`}>
             <div className="auth-container-2">
                 <img src={icon} alt="stack overflow" className="login-logo" />
                 <form onSubmit={handleSubmit}>
