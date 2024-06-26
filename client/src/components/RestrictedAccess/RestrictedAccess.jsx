eb28143642587b6a928ba8df23e41300e567bf16
6ft-alien <nityanand.exe@gmail.com>
2024-06-26 22:03:35 +0530

    Update RestrictedAccess.jsx

--- client/src/components/RestrictedAccess/RestrictedAccess.jsx
+++ client/src/components/RestrictedAccess/RestrictedAccess.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './RestrictedAccess.css';
 import warning from '../../assets/warning.svg';
 import { useTranslation } from 'react-i18next';
+import { useLocation } from 'react-router-dom';
 
 const RestrictedAccess = () => {
+  const location = useLocation();
   const { t } = useTranslation();
+  const [language, setLanguage] = useState('');
+
+  useEffect(() => {
+    const lang = localStorage.getItem('language');
+    setLanguage(lang);
+  }, [location]);
 
   return (
-    <section className="message-section">
+    <section className={`message-section ${language}`}>
       <div className="message-container">
         <div className='message-container-2'>
           <h4 className='message'>
